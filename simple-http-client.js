c46a78bc39532037afaa9677f741c1cbf702b309
Xavier Milliard <xm@faascape.com>
2016-03-08 10:53:36 +0100

    add stream management (src formatting :-( )

--- simple-http-client.js
+++ simple-http-client.js
@@ -7,97 +7,110 @@ var stream = require('stream');
 var DEFAULT_TARGET_HOST = '127.0.0.1';
 var DEFAULT_TARGET_PROTOCOL = 'http';
 
-var Client = exports.Client = function(options){
-  var config = {};
-  if(typeof options == 'string') {
-      var parsedUrl = url.parse(options);
-      config.targetHost = parsedUrl.hostname;
-      if(parsedUrl.port) {
-        config.targetPort = parseInt(parsedUrl.port);
-      }
-      config.targetProtocol = parsedUrl.protocol;
-  }
-  else {
-    config = options || { };
-  }
-  this.targetHost =  config.targetHost|| DEFAULT_TARGET_HOST;
-  this.targetPort =  config.targetPort;
-  var protocol = config.targetProtocol || DEFAULT_TARGET_PROTOCOL;
-  this.targetProtocol = protocol == 'http'|| protocol == 'http:' ? http : https;
-};
-
-Client.prototype._doRequest = function(token, method, path, appHeaders, payload, cb) {
-  if(!cb) {
-    cb = payload;
-    payload = '';
-  }
-  if(!cb) {
-    cb = appHeaders;
-    appHeaders = null;
-  }
-  
-  var options = {
-    host:this.targetHost,
-    port:this.targetPort,
-    path:path,
-    method:method,
-    rejectUnauthorized:false,
-    headers:{
+var Client = exports.Client = function (options) {
+    var config = {};
+    if (typeof options == 'string') {
+        var parsedUrl = url.parse(options);
+        config.targetHost = parsedUrl.hostname;
+        if (parsedUrl.port) {
+            config.targetPort = parseInt(parsedUrl.port);
+        }
+        config.targetProtocol = parsedUrl.protocol;
     }
-  };
-
-  if(token) {
-    options.headers['Authorization'] = 'Bearer '+token;
-  }
-  if(appHeaders) {
-    var propertyName;
-    for(propertyName in appHeaders) {
-      options.headers[propertyName] = appHeaders[propertyName];
+    else {
+        config = options || {};
     }
-  }
+    this.targetHost = config.targetHost || DEFAULT_TARGET_HOST;
+    this.targetPort = config.targetPort;
+    var protocol = config.targetProtocol || DEFAULT_TARGET_PROTOCOL;
+    this.targetProtocol = protocol == 'http' || protocol == 'http:' ? http : https;
+};
 
-  
-  if(payload && !options.headers['Content-Type']) {
-    options.headers[ 'Content-Type'] = 'application/json';
-  }
+Client.prototype._doRequest = function (token, method, path, appHeaders, payload, writeStream, cb) {
+    if (!cb) {
+        cb = writeStream;
+        writeStream = null;
+    }
+    if (!cb) {
+        cb = payload;
+        payload = '';
+    }
+    if (!cb) {
+        cb = appHeaders;
+        appHeaders = null;
+    }
 
-  var result = '';
-  var req = this.targetProtocol.request(options, function(res) {
-    res.on('data', function(chunk) {
-      result += chunk;
+    var options = {
+        host: this.targetHost,
+        port: this.targetPort,
+        path: path,
+        method: method,
+        rejectUnauthorized: false,
+        headers: {
+        }
+    };
+
+    if (token) {
+        options.headers['Authorization'] = 'Bearer ' + token;
+    }
+    if (appHeaders) {
+        var propertyName;
+        for (propertyName in appHeaders) {
+            options.headers[propertyName] = appHeaders[propertyName];
+        }
+    }
+
+
+    if (payload && !options.headers['Content-Type']) {
+        options.headers['Content-Type'] = 'application/json';
+    }
+
+    var result = '';
+    var req = this.targetProtocol.request(options, function (res) {
+        if (writeStream) {
+            res.pipe(writeStream);
+        }
+        else {
+            res.on('data', function (chunk) {
+                result += chunk;
+            });
+        }
+        res.on('end', function () {
+            cb(null, res, result);
+        });
     });
-    res.on('end', function() {
-      cb(null, res, result);
+    req.on('error', function (err) {
+        console.log('err : ', err);
+        cb(err);
     });
-  });
-  req.on('error', function(err) {
-    console.log('err : ', err);
-    cb(err);
-  });
-  
-  if(payload instanceof stream.Readable) {
-      payload.pipe(req);
-  } else {
-    var dataToSend = typeof payload == 'object' ? JSON.stringify(payload) : payload;
-    req.end(dataToSend);    
-  }
+
+    if (payload instanceof stream.Readable) {
+        payload.pipe(req);
+    } else {
+        var dataToSend = typeof payload == 'object' ? JSON.stringify(payload) : payload;
+        req.end(dataToSend);
+    }
 
 };
 
-Client.prototype.doPost = function(token, path, appHeaders, payload, cb) {
-  this._doRequest(token, 'POST', path, appHeaders, payload, cb);
+Client.prototype.doPost = function (token, path, appHeaders, payload, cb) {
+    this._doRequest(token, 'POST', path, appHeaders, payload, cb);
 };
 
-Client.prototype.doPut = function(token, path, appHeaders, payload, cb) {
-  this._doRequest(token, 'PUT', path, appHeaders, payload, cb);
+Client.prototype.doPut = function (token, path, appHeaders, payload, cb) {
+    this._doRequest(token, 'PUT', path, appHeaders, payload, cb);
 };
 
-Client.prototype.doDelete = function(token, path, appHeaders, cb) {
-  this._doRequest(token, 'DELETE', path, appHeaders, cb);
+Client.prototype.doDelete = function (token, path, appHeaders, cb) {
+    this._doRequest(token, 'DELETE', path, appHeaders, cb);
 };
 
-Client.prototype.doGet = function(token, path, appHeaders, cb) {
-  this._doRequest(token, 'GET', path, appHeaders, cb);
+Client.prototype.doGet = function (token, path, appHeaders, cb) {
+    this._doRequest(token, 'GET', path, appHeaders, cb);
+};
+
+Client.prototype.doGet2Stream = function (token, path, appHeaders, ws, cb) {
+    this._doRequest(token, 'GET', path, appHeaders, '', ws, cb);
 };
 
 exports.module = Client;
--- tests/tests.js
+++ tests/tests.js
@@ -5,114 +5,137 @@ var Client = require('../simple-http-client.js').Client;
 
 var PAYLOAD1 = "SERVER1";
 
-describe("test http client", function() {
-	
-	
-	var server1;
-	var server2;
+describe("test http client", function () {
+
+
+    var server1;
+    var server2;
     var server3;
-	
-	before(function() {
-        
-		server1 = http.createServer(function(req, res) {
-			res.end(PAYLOAD1);
-		}).listen(8000, "127.0.0.1");
-        
-		server2 = http.createServer(function(req, res) {
-			var content = '';
-			req.on('data', function(chunk) {
-				content += chunk;
-			});
-			req.on('end', function() {
-				res.end(content);	
-			});
-		}).listen(8001, "127.0.0.1");
-        
-		server3 = http.createServer(function(req, res) {
-			var size = 0;
-			req.on('data', function(chunk) {
-				size += chunk.length;
-			});
-			req.on('end', function() {
-				res.end(JSON.stringify({size:size}));	
-			});
-		}).listen(8002, "127.0.0.1");
-        
-     });
-       
+    var server4;
 
-	describe("GET / with endpoint", function() {
+    before(function () {
+
+        server1 = http.createServer(function (req, res) {
+            res.end(PAYLOAD1);
+        }).listen(8000, "127.0.0.1");
+
+        server2 = http.createServer(function (req, res) {
+            var content = '';
+            req.on('data', function (chunk) {
+                content += chunk;
+            });
+            req.on('end', function () {
+                res.end(content);
+            });
+        }).listen(8001, "127.0.0.1");
+
+        server3 = http.createServer(function (req, res) {
+            var size = 0;
+            req.on('data', function (chunk) {
+                size += chunk.length;
+            });
+            req.on('end', function () {
+                res.end(JSON.stringify({ size: size }));
+            });
+        }).listen(8002, "127.0.0.1");
+
+        server4 = http.createServer(function (req, res) {
+            res.end("teststream1");
+        }).listen(8003, "127.0.0.1");
+
+
+
+    });
+
+
+    describe("GET / with endpoint", function () {
         var client = new Client("http://127.0.0.1:8000");
-		it('should retrieve payload from get request', function(done1) {
-			client.doGet(null, '/', function(err, res, result) {
-				assert.equal(result, PAYLOAD1);
-				done1();
-			});			
-		});
+        it('should retrieve payload from get request', function (done1) {
+            client.doGet(null, '/', function (err, res, result) {
+                assert.equal(result, PAYLOAD1);
+                done1();
+            });
+        });
 
-	});
+    });
 
 
-	
-	describe("GET / with options", function() {
-		var client = new Client({
-			targetPort:8000
-		});
-		it('should retrieve payload from get request', function(done1) {
-			client.doGet(null, '/', function(err, res, result) {
-				assert.equal(result, PAYLOAD1);
-				done1();
-			});			
-		});
 
-	});
-	
+    describe("GET / with options", function () {
+        var client = new Client({
+            targetPort: 8000
+        });
+        it('should retrieve payload from get request', function (done1) {
+            client.doGet(null, '/', function (err, res, result) {
+                assert.equal(result, PAYLOAD1);
+                done1();
+            });
+        });
 
-	
-	describe("POST /", function() {
-		var client = new Client({
-			targetPort:8001
-		});
-		var payload = {
-			var1:"value1"
-		};
-		
-		it('should respond with posted content', function(done2) {
-			client.doPost(null, '/', null, payload, function(err, res, result) {
-				var parsedResult = JSON.parse(result);
-				assert.equal(parsedResult.var1, "value1");
-				done2();
-			});			
-		});
+    });
 
-	});
-	
-    
-	describe("POST / with stream", function() {
-		var client = new Client({
-			targetPort:8002
-		});
-		
-		it('should respond with posted content size', function(done2) {
+
+
+    describe("POST /", function () {
+        var client = new Client({
+            targetPort: 8001
+        });
+        var payload = {
+            var1: "value1"
+        };
+
+        it('should respond with posted content', function (done2) {
+            client.doPost(null, '/', null, payload, function (err, res, result) {
+                var parsedResult = JSON.parse(result);
+                assert.equal(parsedResult.var1, "value1");
+                done2();
+            });
+        });
+
+    });
+
+
+    describe("POST / with stream", function () {
+        var client = new Client({
+            targetPort: 8002
+        });
+
+        it('should respond with posted content size', function (done2) {
             var stat = fs.statSync('package.json');
             var is = fs.createReadStream('package.json');
-			client.doPost(null, '/', null, is, function(err, res, result) {
-				var parsedResult = JSON.parse(result);
-				assert.equal(parsedResult.size, stat.size);
-				done2();
-			});			
-		});
+            client.doPost(null, '/', null, is, function (err, res, result) {
+                var parsedResult = JSON.parse(result);
+                assert.equal(parsedResult.size, stat.size);
+                done2();
+            });
+        });
 
-	});
-	
-    
-    
-    
-	
-	after(function() {
-		server1.close();
-		server2.close();
-		server3.close();        
     });
-	
+
+    describe("GET / to stream", function () {
+        var client = new Client({
+            targetPort: 8003
+        });
+
+        it('should store content to file', function (done2) {
+            var ws = fs.createWriteStream('teststream1');
+            client.doGet2Stream(null, '/', null, ws, function (err, res, result) {
+                ws.close();
+                var testContent = fs.readFileSync("teststream1");
+                assert.equal("teststream1", testContent);
+                done2();
+            });
+        });
+
+    });
+
+
+
+
+    after(function () {
+        server1.close();
+        server2.close();
+        server3.close();
+    });
+
 });
